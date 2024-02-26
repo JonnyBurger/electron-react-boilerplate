795a04dcf2c2bd0c4f615301814c67fef769d7e9
Jonny Burger <jonathanburger11@gmail.com>
2024-02-26 12:05:35 +0100

    add remotion in dev

--- src/main/main.ts
+++ src/main/main.ts
@@ -12,6 +12,7 @@ import path from 'path';
 import { app, BrowserWindow, shell, ipcMain } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
+import { renderMedia, selectComposition } from '@remotion/renderer';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 
@@ -26,9 +27,17 @@ class AppUpdater {
 let mainWindow: BrowserWindow | null = null;
 
 ipcMain.on('ipc-example', async (event, arg) => {
-  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-  console.log(msgTemplate(arg));
-  event.reply('ipc-example', msgTemplate('pong'));
+  const selected = await selectComposition({
+    serveUrl: 'https://watercolor-map.vercel.app/?/MapAnimation',
+    id: 'MapAnimation',
+  });
+
+  await renderMedia({
+    codec: 'h264',
+    composition: selected,
+    serveUrl: 'https://watercolor-map.vercel.app/?/MapAnimation',
+    logLevel: 'verbose',
+  });
 });
 
 if (process.env.NODE_ENV === 'production') {
